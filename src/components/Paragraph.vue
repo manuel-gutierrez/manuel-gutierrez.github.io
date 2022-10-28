a40f29aebf43be85281948f21ce846aaca22bd14
Manuel Gutierrez <m.gutierrez@druo.com>
2022-10-28 15:46:58 -0500

    fix: copy

--- src/components/Paragraph.vue
+++ src/components/Paragraph.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="col-12 profile" >
-    <p>Soy Siro tu manzana Latina, <b>Genie Bar Certificado</b>.</p>
-    <p>Si no entiendes que es eso te lo explican aquí: <a href="https://www.applesfera.com/apple/especial-apple-store-todo-lo-que-necesitas-saber-sobre-el-genius-bar" target="_blank">explican aquí</a> ;). 
-    osea el sueño de Steve (Nota -- mi ídolo de quien inspire mi imagen en la bio.) así que aquí estoy: para servirte.</p>
-    <p>Identificarme es fácil ya que siempre estoy con la más linda (Barbie) del lugar. <span>Mi "objetivo", tu felicidad!</span> </p>  
-    
-    <p>Me necesitas? <b>Oye Siro!</b></p>
 
+    <h3>Sobre mí: </h3>
+    <p> No soy Steve, en mi foto soy el. <br>
+        No soy Ken, pero estoy con Barbie. <br>
+        No soy Android, pero si iOS.<br>
+        No soy IT, pero si <a href="https://www.applesfera.com/apple/especial-apple-store-todo-lo-que-necesitas-saber-sobre-el-genius-bar" target="_blank"> Apple Genie. </a> 
+    </p>
+    <h4>Solo dí:</h4>
+    <p> Ey Siro!</p>
+    <span> </span>
 
   </div>
 </template>
@@ -16,13 +19,15 @@
 p {
   margin-top: 10px;
 }
+
 a {
   color: #fff;
 }
-h2{
+
+h2, h3{
   font-weight: bold;
 }
-span{
+h4 {
   text-decoration: underline;
 }
 </style>
--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="col-12">
     <h1 class="row name">Siro</h1>
-    <h2 class="row title">Tech Genie</h2>
+    <h2 class="row title">Tech Genie Certificado</h2>
   </div>
 </template>
 
